f7a75636686a5282773db33581863f282766de75
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-18 15:55:20 +0530

    resolving errors

---
+++ app.js
@@ -0,0 +1,26 @@
+const name="tushar sharma"
+let age=10
+const fun=(name,age)=>{
+  console.log(name,age)
+}
+
+// fun(name,age)
+// var i=10
+
+// while(i<=20){
+//   console.log(i++);
+// }
+
+
+let x=10
+
+switch(x){
+  
+  case 15:
+    console.log(x)
+  case 20:
+    console.log(20)
+  default:
+    console.log("this is the default case")
+
+}
