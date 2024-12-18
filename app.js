346105881c79dd972a6f84d8c9c6a273dbfe8d4b
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-18 17:13:26 +0530

    studied js loops and opeartors

---
+++ app.js
@@ -0,0 +1,52 @@
+// const obj={
+//     name:"tushar sharma",
+//     age:25,
+//     city:"Indore"
+// }
+
+// for(let key in obj){
+//     console.log(`${key}:${obj[key]}`)
+// }
+
+// const arr=["tushar sharma",16,"paris"]
+
+// for(let key of arr){
+//     console.log(key)
+// }
+
+let x=10;
+let y=25;
+
+// const res=((x+y)*x)/y;
+
+// x++;
+// y--;
+
+// console.log(x+y);
+
+// if(x!==y){
+//     console.log("yes they are not equl")
+// }
+
+// if(x>y){
+//     console.log("yes")
+// }else{
+//     console.log("no")
+// }
+
+// x=0
+// y=1
+
+// if(x&&y){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+// if(x||y){
+//     console.log(true)
+// }else{
+//     console.log(false)
+// }
+
+x==y?console.log(true):console.log(false)
