3e5dbbaa9fc90540580a7b7356b7b50df82f6acc
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-19 14:41:29 +0530

    added js events

--- app.js
+++ app.js
@@ -1,52 +1,48 @@
-// const obj={
-//     name:"tushar sharma",
-//     age:25,
-//     city:"Indore"
+// function nitesh_sir(){//normal function
+
+//     console.log("good morning my name is nitesh and i am your mentor")
+
 // }
 
-// for(let key in obj){
-//     console.log(`${key}:${obj[key]}`)
+// const tushar=()=>{ //arrow function from es6/7
+//     console.log("good morning my name is tushar and i am a trainee at hotbit infosoft pvt ltd")
 // }
 
-// const arr=["tushar sharma",16,"paris"]
+// nitesh_sir()
+// tushar()
 
-// for(let key of arr){
-//     console.log(key)
+
+// setTimeout(()=>{
+
+//     let element=document.getElementById("hOne").innerText="this has been read"
+// },3000)
+//types of events in js
+//onclick
+//onchange
+//oninput
+//onunload
+//onload
+//onmouseover
+//onmouseout
+//onmousedown
+//onmouseup
+
+// const changeText=()=>{
+//     const element=document.getElementById("hOne")
+//     element.innerText="Thankyou"
 // }
 
-let x=10;
-let y=25;
+const form=document.querySelector("#userform")
+const input=document.querySelector("#username") 
+const list=document.querySelector("#userlist")
 
-// const res=((x+y)*x)/y;
+form.addEventListener("submit",(e)=>{
 
-// x++;
-// y--;
+    e.preventDefault()
+    const val=input.value
+    const li=document.createElement("li")
+    li.innerText=val
+    list.appendChild(li)
 
-// console.log(x+y);
+})
 
-// if(x!==y){
-//     console.log("yes they are not equl")
-// }
-
-// if(x>y){
-//     console.log("yes")
-// }else{
-//     console.log("no")
-// }
-
-// x=0
-// y=1
-
-// if(x&&y){
-//     console.log(true)
-// }else{
-//     console.log(false)
-// }
-
-// if(x||y){
-//     console.log(true)
-// }else{
-//     console.log(false)
-// }
-
-x==y?console.log(true):console.log(false)
