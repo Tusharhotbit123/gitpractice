9c8392c2ca770a104b4f3634a6098709db211e4c
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-20 15:08:25 +0530

    did all object methods

--- app.js
+++ app.js
@@ -8,5 +8,10 @@ function Person(name,age,height,eyecolor){
 }
 
 const Tushar=new Person("Tushar Sharma",23,"6 ft","black")
+const Clone={name:"Clone"}
 
-console.log(Tushar)
+console.log(Object.keys(Tushar))
+console.log(Object.values(Tushar))
+console.log(Object.entries(Tushar))
+console.log(Object.assign(Clone,Tushar))
+console.log(Object.freeze(Tushar))
