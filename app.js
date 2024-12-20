db3be19a1e2ae369db57ad727177d36a66960bc1
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-20 11:18:54 +0530

    added changes for day5

--- app.js
+++ app.js
@@ -1,48 +1,10 @@
-// function nitesh_sir(){//normal function
+const first=document.getElementById("container")
+const second=document.getElementById("container2")
+const third=document.getElementById("button")
 
-//     console.log("good morning my name is nitesh and i am your mentor")
+  second.addEventListener("click",(e)=>{
 
-// }
-
-// const tushar=()=>{ //arrow function from es6/7
-//     console.log("good morning my name is tushar and i am a trainee at hotbit infosoft pvt ltd")
-// }
-
-// nitesh_sir()
-// tushar()
+    console.log(e.target)
 
 
-// setTimeout(()=>{
-
-//     let element=document.getElementById("hOne").innerText="this has been read"
-// },3000)
-//types of events in js
-//onclick
-//onchange
-//oninput
-//onunload
-//onload
-//onmouseover
-//onmouseout
-//onmousedown
-//onmouseup
-
-// const changeText=()=>{
-//     const element=document.getElementById("hOne")
-//     element.innerText="Thankyou"
-// }
-
-const form=document.querySelector("#userform")
-const input=document.querySelector("#username") 
-const list=document.querySelector("#userlist")
-
-form.addEventListener("submit",(e)=>{
-
-    e.preventDefault()
-    const val=input.value
-    const li=document.createElement("li")
-    li.innerText=val
-    list.appendChild(li)
-
-})
-
+  })
