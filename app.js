7cab22c52d7f7af5f82d5e68558a4afec43655f0
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-23 14:50:26 +0530

    studied methods of array searching

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-// const arr=[9,8,7,1,2,3,4,5,6]
+const arr=[1,2,3,4,5,6]
 
-// console.log(arr.sort())//this method is used to sort the array
+// let idx=arr.indexOf(3,-1)//used to search the index of a value in array frmo left to right
+
+// console.log(idx)
+
+// const val=arr.find(callback)
+
+// function callback(num){//returns the first value with a particular condition
+//     return num>3
+// }
+
+// console.log(val)
+
+// const idx=arr.findIndex((num)=>{//returns an index value for first element that satisfies the condition
+//     return num>3
+// })
+
+// console.log(idx)
+
+// const val=arr.includes(7);//checks if a vlaue is contained and returns true or false based on that
+
+// console.log(val)
