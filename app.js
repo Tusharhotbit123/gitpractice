5ab7092bbd14fbf6704b7c7e6327596ec2f2b3c8
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-23 16:35:33 +0530

    studied js promises

--- app.js
+++ app.js
@@ -1,26 +1,17 @@
-const arr=[1,2,3,4,5,6]
+const promise=new Promise((resolve,reject)=>{
 
-// let idx=arr.indexOf(3,-1)//used to search the index of a value in array frmo left to right
+    let error=false;
 
-// console.log(idx)
+    if(error){
+        reject("error:there is an error")
+    }else{
+        resolve({username:"tushar",email:"tushar@example.com"})
+    }
+    
+})
 
-// const val=arr.find(callback)
-
-// function callback(num){//returns the first value with a particular condition
-//     return num>3
-// }
-
-// console.log(val)
-
-// const idx=arr.findIndex((num)=>{//returns an index value for first element that satisfies the condition
-//     return num>3
-// })
-
-// console.log(idx)
-
-// const val=arr.includes(7);//checks if a vlaue is contained and returns true or false based on that
-
-// console.log(val)
-
-// const newarr=[...arr,7,8,9,10];//spreadopeartor
-// console.log(newarr)
+promise.then(function(user){
+    console.log(user.email)
+}).catch(function(err){
+    console.log(err)
+})
