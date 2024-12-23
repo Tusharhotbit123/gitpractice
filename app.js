38065488cb3ba46cd3c78bb428bd0f21bfa2d708
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-23 13:05:45 +0530

    array sort method

--- app.js
+++ app.js
@@ -1,27 +1,3 @@
-const arr=[1,2,3,4,5,6]
+// const arr=[9,8,7,1,2,3,4,5,6]
 
-// const newarr=arr.map((num)=>{//map is used to iterate over an array and create a new array
-//     return(
-//         num*10
-//     )
-// })
-
-// const newarr=arr.filter((num)=>{//filter is used to check a certain condition and return a new array
-//     return(
-//          num<=3
-//     )
-// })
-
-// let sum=0;
-
-// const callback=(num)=>{
-//   sum+=num
-// }
-
-// arr.forEach(callback)//used to iterate over each element and provide some callback function,doesnot create a new function
-
-// const total=arr.reduce((sum,curr,idx,arr)=>{//used to iterate over a sample array and return a reduced value
-//     return sum+arr[idx]
-// })
-
-console.log(total)
+// console.log(arr.sort())//this method is used to sort the array
