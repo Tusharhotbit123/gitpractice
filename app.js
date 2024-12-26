39247002c4944d1259c2719d46d601184bb9cecc
Tushar Sharma <tsharma.hotbitinfosoft.com>
2024-12-26 12:00:57 +0530

    closures

--- app.js
+++ app.js
@@ -1,25 +1,17 @@
-//prototypes
+//closures
+//closures is a function bundled with its lexical scope
 
-const object={
-    name:"Nitesh",
-    city:"Indore"
+function x(){
+
+    var a=7;
+
+    function y(){
+        console.log(a)
+    }
+
+    return y;
+
 }
 
-const object2={
-    name:"Tushar",
-}
-
-//must avoid this due to performance issues
-object2.__proto__=object//assigning object as prototype to object2
-
-// console.log(object2.city)
-
-Function.prototype.mybind=function(){
-    console.log("ahdaksjd")
-}
-
-function fun(){
-    console.log("check")
-}
-
-console.log(fun.__proto__.mybind())
+var z=x()
+z()//function y remembers its surrounding and hence even after x has finished its execution,it still works and we can print a 
